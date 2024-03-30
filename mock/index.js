4e36da1ee4841b1cfcb509a610cfcbfaa4df7835
luminous <910667665@qq.com>
2024-03-30 22:36:24 +0800

    feat：实现上传图片接口

--- mock/index.js
+++ mock/index.js
@@ -3,7 +3,8 @@ const cascader = require("../mock/cascader");
 const dialog = require("../mock/dialog");
 const fileUpload = require("../mock/fileUpload");
 const tabs = require("../mock/tabs");
+const uploadImg = require("../mock/uploadImg");
 
-const mockList = [...test, ...cascader, ...dialog, ...fileUpload, ...tabs];
+const mockList = [...test, ...cascader, ...dialog, ...fileUpload, ...tabs, ...uploadImg];
 
 module.exports = mockList;
